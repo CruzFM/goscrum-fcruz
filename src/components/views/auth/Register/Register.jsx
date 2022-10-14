965e4c161783104f5db7651126459aa3766ca305
Fernando Cruz <cruzfernandomartin94@gmail.com>
2022-10-14 07:50:12 -0300

    mui and uuid installed. switch added. conditional rendering of team ID added.

--- src/components/views/auth/Register/Register.jsx
+++ src/components/views/auth/Register/Register.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { Formik, Field, Form, FormikProps, validateYupSchema, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
+import { Switch, FormControlLabel } from '@mui/material';
 
 export const Register = () => {
     const LoginSchema = Yup.object().shape({
@@ -33,115 +34,199 @@ export const Register = () => {
         .then(data => {
           setData(data.result);
         })
-    }, [])
+    }, []);
+    
+    const handleChangeContinent = (value, setFieldValue) => {
+      setFieldValue('continent', value)
+      if(value !== 'America'){
+        setFieldValue('region', 'otro')
+      }
+    }
 
     return (
       <div className="auth">
         <h1>Register here</h1>
 
         <Formik
-          initialValues={ {email: '', password: ''} }
+          initialValues={{
+            name: "",
+            email: "",
+            password: "",
+            role: "",
+            teamID: "",
+            continent: "",
+            region: "",
+            switch: false,
+          }}
           validationSchema={LoginSchema}
-          onSubmit={ (event, values) =>{ 
-            event.preventDefault()
-            console.log(values) }
-          }
+          onSubmit={(values) => {
+            // event.preventDefault();
+            // console.log(values);
+            alert('hola Fer, lo lograste')
+          }}
         >
-          {
-            ({touched, errors, isSubmitting, handleBlur, values}) =>(
-              <Form>
-                <div className='form-control'>
-                  <label htmlFor='name'>User Name</label>
-                  <Field 
-                    type='input'
-                    name='name'
-                    placeholder='User Name.'
-                    onBlur={handleBlur}
-                    className={errors.name  && touched.name ? 'error-field' : ''}
-                  />
-                  {errors.name && touched.name && <div className='errors'>{errors.name}</div>}
-                </div>
-                <div className='form-control'>
-                  <label htmlFor='password'>Password</label>
-                  <Field 
-                    type='password'
-                    name='password'
-                    placeholder='Password.'
-                    onBlur={handleBlur}
-                    className={errors.password && touched.password ? 'error-field' : ''}
-                  />
-                  {errors.password && touched.password && <div className='errors'>{errors.password}</div>}
-                </div>
-                <div className='form-control'>
-                  <label htmlFor='email'>Email</label>
-                  <Field 
-                    type='email'
-                    name='email'
-                    placeholder='Email.'
-                    onBlur={handleBlur}
-                    className={errors.email && touched.email ? 'error-field' : ''}
-                  />
-                  {errors.email && touched.email && <div className='errors'>{errors.email}</div>}
-                </div>
-                <div className='form-control'>
-                  <label htmlFor='role'>Role</label>
-                  <Field 
-                    as='select'
-                    name='role'
-                    onBlur={handleBlur}
-                    className={errors.role && touched.role ? 'error-field' : ''}
-                  >
-                    <option value="">Select your option</option>
-                    {data?.Rol?.map(role => <option value={role} key={role}>{role}</option>)}
-                  </Field>
-                  {errors.role && touched.role && <div className='errors'>{errors.role}</div>}
-                </div>
-                <div className='form-control'>
-                  <label htmlFor='continent'>Continent</label>
-                  <Field 
-                    as='select'
-                    name='continent'
-                    onBlur={handleBlur}
-                    className={errors.continent  && touched.continent ? 'error-field' : ''}
-                  >
-                    <option value="">Select your Continent</option>
-                    {data?.continente?.map(continent => <option value={continent} key={continent}>{continent}</option>)}
-                  </Field>
-                  {errors.continent && touched.continent && <div className='errors'>{errors.continent}</div>}
-                </div>
-
-                {
-                  values.continent === 'America' &&
-                  (<div className='form-control'>
-                    <label htmlFor='region'>Region</label>
-                    <Field 
-                      as='select'
-                      name='region'
-                      onBlur={handleBlur}
-                      className={errors.region && touched.region ? 'error-field' : ''}
-                    >
-                      <option value="">Select your region</option>
-                      {data?.region?.map(place => <option value={place} key={place}>{place}</option>)}
-                    </Field>
-                    {errors.region && touched.region && <div className='errors'>{errors.region}</div>}
-                  </div>)
-                }
-                
-                <button 
-                  type='submit'
-                  disabled={isSubmitting}
+          {({
+            touched,
+            errors,
+            isSubmitting,
+            handleBlur,
+            values,
+            handleChange,
+            setFieldValue,
+          }) => (
+            <Form>
+              <div className="form-control">
+                <label htmlFor="name">User Name</label>
+                <Field
+                  type="input"
+                  name="name"
+                  placeholder="User Name."
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.name}
+                  className={errors.name && touched.name ? "error-field" : ""}
+                />
+                {errors.name && touched.name && (
+                  <div className="errors">{errors.name}</div>
+                )}
+              </div>
+              <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Password."
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "error-field" : ""
+                  }
+                />
+                {errors.password && touched.password && (
+                  <div className="errors">{errors.password}</div>
+                )}
+              </div>
+              <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Email."
+                  value={values.email}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className={errors.email && touched.email ? "error-field" : ""}
+                />
+                {errors.email && touched.email && (
+                  <div className="errors">{errors.email}</div>
+                )}
+              </div>
+              <div className="form-control">
+                <label htmlFor="role">Role</label>
+                <Field
+                  as="select"
+                  name="role"
+                  value={values.role}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className={errors.role && touched.role ? "error-field" : ""}
                 >
-                  Submit
-                </button>
-                <div>
-                  <h6>Already have an account? </h6>
-                  <Link to='/login'>Login here.</Link>
+                  <option value="">Select your option</option>
+                  {data?.Rol?.map((role) => (
+                    <option value={role} key={role}>
+                      {role}
+                    </option>
+                  ))}
+                </Field>
+                {errors.role && touched.role && (
+                  <div className="errors">{errors.role}</div>
+                )}
+              </div>
+
+              <FormControlLabel
+                control={
+                  <Switch
+                    value={values.switch}
+                    onChange={() =>
+                      setFieldValue("switch", !values.switch)
+                    }
+                    name="switch"
+                    color="secondary"
+                    />
+                }
+                label="Already belong to a team."
+              />
+              {values.switch && 
+              (<div className="form-control">
+                <label htmlFor="teamID">Please, introduce your Team ID</label>
+                <Field
+                  type="text"
+                  name="teamID"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.teamID}
+                />
+              </div>)}
+              <div className="form-control">
+                <label htmlFor="continent">Continent</label>
+                <Field
+                  as="select"
+                  name="continent"
+                  onBlur={handleBlur}
+                  onChange={event => handleChangeContinent(event.currentTarget.value, setFieldValue)}
+                  value={values.continent}
+                  className={
+                    errors.continent && touched.continent ? "error-field" : ""
+                  }
+                >
+                  <option value="">Select your Continent</option>
+                  {data?.continente?.map((continent) => (
+                    <option value={continent} key={continent}>
+                      {continent}
+                    </option>
+                  ))}
+                </Field>
+                {errors.continent && touched.continent && (
+                  <div className="errors">{errors.continent}</div>
+                )}
+              </div>
+
+              {values.continent === "America" && (
+                <div className="form-control">
+                  <label htmlFor="region">Region</label>
+                  <Field
+                    as="select"
+                    name="region"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                    className={
+                      errors.region && touched.region ? "error-field" : ""
+                    }
+                  >
+                    <option value="">Select your region</option>
+                    {data?.region?.map((place) => (
+                      <option value={place} key={place}>
+                        {place}
+                      </option>
+                    ))}
+                  </Field>
+                  {errors.region && touched.region && (
+                    <div className="errors">{errors.region}</div>
+                  )}
                 </div>
-              </Form>
-            )
-          }
+              )}
 
-
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+              <div>
+                <h6>Already have an account? </h6>
+                <Link to="/login">Login here.</Link>
+              </div>
+            </Form>
+          )}
         </Formik>
       </div>
     );
